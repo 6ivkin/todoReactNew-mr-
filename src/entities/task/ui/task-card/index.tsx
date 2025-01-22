bd29a1b65fc7d9234b1af28dbfa119228650b6e3
6ivkin <fedorterentev48@gmail.com>
2025-01-22 23:19:21 +0400

    ui for TaskCard

--- src/entities/task/ui/task-card/index.tsx
+++ src/entities/task/ui/task-card/index.tsx
@@ -1,5 +1,18 @@
 import { Card } from 'antd'
+import { ReactNode } from 'react'
+import { Link } from 'react-router-dom'
 
-export const TaskCard = () => {
-  return <Card />
+type Props = {
+  loading: boolean
+  title: string
+  text: string
+  actions?: ReactNode[]
+}
+
+export const TaskCard = ({ text, title, actions, loading }: Props) => {
+  return (
+    <Card loading={loading} title={title} actions={actions} extra={<Link to='/'>Back</Link>} style={{ width: '300px' }}>
+      {text}
+    </Card>
+  )
 }
